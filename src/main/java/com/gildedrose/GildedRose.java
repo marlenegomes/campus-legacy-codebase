61fc70788e11157b157e9bfc9efbad3816cc8610
marlenegomes <marlenegomes1@gmail.com>
2019-11-04 16:51:27 +0100

    revue code gildedRose class

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,48 +11,48 @@ public class GildedRose {
     Item[] items;
 
     public GildedRose(Item[] items) {
-        for (int i = 0; i < items.length; i++) {
-        logger.info("items: " + items[i]);
-        }
         this.items = items;
     }
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            logger.info("current items: " + items[i].name + "| quality: " + items[i].quality + " | sellIn: " + items[i].sellIn);
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                logger.info("item n'est pas Brie ni Backstage");
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-
-                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                            if (items[i].name.contains("Conjured")) {
-                                items[i].quality = items[i].quality - 2;
-                                logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
-                            } else {
-                                logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
-                                items[i].quality = items[i].quality - 1;
-                                logger.info("Item : " + items[i].name + " | Quality : " + items[i].quality);
-                            }
-                        }
+                        logger.info("Item quality baisse de 1");
+                        items[i].quality = items[i].quality - 1;
                     }
-                }
-            } else {
+                            if (items[i].name.contains("Conjured") && items[i].quality>0) {
+                                logger.info("si conjured : Item quality baisse de 1 en plus");
+                                items[i].quality = items[i].quality - 1;
+                            }
+                    }
+                } else {
+                logger.info("item = brie ou backstage");
                 if (items[i].quality < 50) {
+                    logger.info("item quality augmente de 1");
                     items[i].quality = items[i].quality + 1;
-                    logger.info("item quality: " + items[i].quality);
+
+                    if (items[i].name.contains("Conjured")) {
+                        logger.info("si conjured : Item quality augmente de 1 en plus");
+                        items[i].quality = items[i].quality + 1;
+                    }
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
+                                logger.info("si backstage et <11 : item quality augmente de 1" );
                                 items[i].quality = items[i].quality + 1;
-                                logger.info("item quality: " + items[i].quality);
                             }
                         }
 
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
+                                logger.info("si backstage et <6 : item quality augmente de 1 " + items[i].quality );
                                 items[i].quality = items[i].quality + 1;
-                                logger.info("item quality: " + items[i].quality);
                             }
                         }
                     }
@@ -60,9 +60,8 @@ public class GildedRose {
             }
 
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                logger.info("name item: " + items[i].name );
-                items[i].sellIn = items[i].sellIn - 1;
-                logger.info("item sellIn: " + items[i].sellIn);
+                logger.info("sellIn baisse de 1 " + items[i].sellIn);
+                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
@@ -70,22 +69,30 @@ public class GildedRose {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                                logger.info("item quality baisse de 1 :" + items[i].quality);
                                 items[i].quality = items[i].quality - 1;
-                                logger.info("item quality: " + items[i].quality);
+                            }
+                            if (items[i].name.contains("Conjured")) {
+                                logger.info("si conjured : Item quality baisse de 1 en plus: " + items[i].quality);
+                                items[i].quality = items[i].quality - 1;
                             }
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
-                        logger.info("item quality: " + items[i].quality);
-
+                        logger.info("Backstage quality = 0");
                     }
                 } else {
                     if (items[i].quality < 50) {
+                        logger.info("item quality augmente de 1: " + items[i].quality );
                         items[i].quality = items[i].quality + 1;
-                        logger.info("item quality: " + items[i].quality);
+                    }
+                    if (items[i].name.contains("Conjured")) {
+                        logger.info("si conjured : Item quality augmente de 1 en plus: " + items[i].quality);
+                        items[i].quality = items[i].quality + 1;
                     }
                 }
             }
+            logger.info("item name: " + items[i].name  + "| item quality: " + items[i].quality + "| item sellIn : " + items[i].sellIn);
         }
     }
 
